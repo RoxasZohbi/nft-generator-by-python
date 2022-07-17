316a01b020b31a479b6711cd49e6477dc3063e72
Syed Zohaib Hussain <roxas.zohbi786@gmail.com>
2022-07-17 20:21:57 +0500

    init

---
+++ config.py
@@ -0,0 +1,30 @@
+"""
+Fill in your information in the configuration variables below
+"""
+folder = "Demo-2"
+# traits = [
+#     "Background", 
+#     "Pumpkin Color",
+#     "Headwear",
+#     "Mouth",
+#     "Nose",
+#     "Extras",
+#     "Eyes"
+# ]
+traits = [
+    "body", 
+    "clothes",
+    "face",
+    "facehairs",
+    "hairs",
+    "shoes",
+] # The different layers and the order that they will be used - MUST be same as trait layer folders
+imageCount = 100 # Total number of images to create
+nameFormat = "NFT #[NUMBER]" # The name of each NFT - '[NUMBER]' will be replaced with the NFT number
+description = "Really awesome description!" # Description of collection
+royalty = 2.5 # Royalty percentage
+royaltyAddress = "6R1YMnnjWsj93a438whWkimCimNzAgKRid7QkUfpkCxF" # Address to pay the royalties to
+collectionName = "Cool collection" # Name of collection
+collectionFamily = "Cool collection" # Name of family (often same as collection)
+symbol = "" # Symbol (often blank)
+blockchain = "ETH" # ETH or SOL
